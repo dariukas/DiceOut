b9bda921442ad42e20fb7ec02c33be536cfe990a
dariukas <dariui.miliauskui@gmail.com>
2017-03-16 23:29:44 +0200

    Initial Commit

---
+++ DiceOut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiceOut
+//
+//  Created by Kristina Šlekytė on 16/03/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiceOut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiceOut
+//
+//  Created by Kristina Šlekytė on 16/03/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
